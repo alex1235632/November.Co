f279c1f896ee6b40c656f9bcfe93cf2af18f6df4
alex1235632 <abdullahmoh4200@gmail.com>
2026-03-23 21:47:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -328,5 +328,35 @@ async def jl5(interaction: discord.Interaction):
 ⠀⠀⠉⠉⠙⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠁⠛⠛⠛⠛⠛⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁"""
     await interaction.response.send_message(f"```{art}```")
 
-bot.run(TOKEN)
+bot.run("MTQ4MzUxODk2OTEyNTAxMTU4Ng.GUp116.9QZkUZIihDQuuLSfTS757b9LMwdtpC5FQQCvyE")
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send(f"Joined {channel}")
+    else:
+        await ctx.send("You are not in a voice channel!")
+
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Disconnected")
+    else:
+        await ctx.send("I am not in a voice channel")
+
+bot.run("MTQ4MzUxODk2OTEyNTAxMTU4Ng.GUp116.9QZkUZIihDQuuLSfTS757b9LMwdtpC5FQQCvyE")
